a70c0bb55120084a41e30689156f582a19cd6a96
Alexander Storbeck <alexst2768@gmail.com>
2021-06-12 15:52:48 +0200

    [ADD] added Processing lib and fitst test Sketch

--- app/src/main/java/com/example/projectsnakereloaded/MainActivity.java
+++ app/src/main/java/com/example/projectsnakereloaded/MainActivity.java
@@ -3,21 +3,49 @@ package com.example.projectsnakereloaded;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import processing.android.CompatUtils;
+import processing.android.PFragment;
+import processing.core.PApplet;
 
 public class MainActivity extends AppCompatActivity {
 
+    private PApplet sketch;
+
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (sketch != null) {
+            sketch.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        // branch test
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        FrameLayout frame = new FrameLayout(this);
+        frame.setId(CompatUtils.getUniqueViewId());
+        setContentView(frame, new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
+
+        sketch = new Sketch();
+        PFragment fragment = new PFragment(sketch);
+        fragment.setView(frame, this);
+        //setContentView(R.layout.activity_main);
 
 
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        if (sketch != null) {
+            sketch.onNewIntent(intent);
+        }
     }
 }

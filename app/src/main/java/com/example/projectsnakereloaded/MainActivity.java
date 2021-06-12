733088e10dbdc66ddd963f49586bb94eef9a0069
Luca <luca.jetter@hochschule-stralsund.de>
2021-06-12 13:39:18 +0200

    [ADD] onRequestPermissionsResult added

--- app/src/main/java/com/example/projectsnakereloaded/MainActivity.java
+++ app/src/main/java/com/example/projectsnakereloaded/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.projectsnakereloaded;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,8 +8,14 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
